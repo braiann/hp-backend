ddef16773f23758d861b5830f2a83db3cbbfad6d
Nahuel Tabasso <nahueltabasso@gmail.com>
2021-12-18 12:49:24 -0300

    PersonControllerTest

--- src/test/java/hp/server/app/tests/BaseTest.java
+++ src/test/java/hp/server/app/tests/BaseTest.java
@@ -1,13 +1,23 @@
 package hp.server.app.tests;
 
 import hp.server.app.models.dto.request.LoginRequestDTO;
+import hp.server.app.models.dto.response.JwtResponseDTO;
 import hp.server.app.models.entity.Address;
 import hp.server.app.models.entity.City;
 import hp.server.app.models.entity.Person;
 import hp.server.app.models.entity.Province;
-import hp.server.app.models.repository.CityRepository;
-import hp.server.app.models.repository.ProvinceRepository;
+import hp.server.app.models.repository.*;
+import hp.server.app.services.PersonService;
+import nrt.common.microservice.exceptions.CommonBusinessException;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
 
@@ -30,15 +40,58 @@ public class BaseTest {
     protected static final Long PROVINCE_ID  = 1L;
     protected static final Long CITY_ID = 1L;
     protected static final String ADDRESS_PERSON = "AddressTest";
+    protected Long maxPersonId;
     protected Long PERSON_ID;
     protected Person person;
     protected String accessToken;
     protected String refreshToken;
+    private Person loginPerson;
 
+    @Autowired
+    protected RestTemplate restTemplate;
+    @Autowired
+    protected PasswordEncoder passwordEncoder;
+    @Autowired
+    protected PersonService personService;
+    @Autowired
+    protected PersonRepository personRepository;
     @Autowired
     protected ProvinceRepository provinceRepository;
     @Autowired
     protected CityRepository cityRepository;
+    @Autowired
+    protected RoleRepository roleRepository;
+    @Autowired
+    protected RefreshTokenRepository refreshTokenRepository;
+
+
+    @BeforeAll
+    public void setUp() {
+        // Save new user and then generate a jwt token
+        try {
+            loginPerson = buildPerson();
+            loginPerson = personService.save(loginPerson);
+            maxPersonId = loginPerson.getId();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        LoginRequestDTO loginRequestDTO = new LoginRequestDTO(USERNAME_PERSON, PASSWORD_PERSON);
+
+        String url = REST_API_PATH + "/auth/signin";
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity<Object> entity = new HttpEntity<>(loginRequestDTO, headers);
+            ResponseEntity<JwtResponseDTO> response = restTemplate.postForEntity(url, entity, JwtResponseDTO.class);
+            JwtResponseDTO jwtResponseDTO = response.getBody();
+            accessToken = jwtResponseDTO.getAccessToken();
+            refreshToken = jwtResponseDTO.getRefreshToken();
+        } catch (HttpClientErrorException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     protected Person buildPerson() {
         person = new Person();
